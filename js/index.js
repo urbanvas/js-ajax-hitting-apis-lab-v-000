1fb6de8783ab35415d76383d6e304671c8ab2fb3
Neil Seejoor <nvseejoor@gmail.com>
2019-01-21 22:00:39 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-function getRepositories() {
+function getRepositories() { // add in user
   const req = new XMLHttpRequest
   req.addEventListener('load', showRepositories)
   req.open('GET', 'https://api.github.com/users/octocat/repos')
