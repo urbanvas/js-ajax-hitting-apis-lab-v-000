e2fd47cee1cfcd818b7910ab67b8c2707817df7b
Neil Seejoor <nvseejoor@gmail.com>
2019-01-21 21:30:23 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ function showRepositories() {
   console.log(repos[2]['name'])
   const repoList = `<ul>${repos.map(r => {`<li>${r.name}</li>`}).join('')}</ul>`
   console.log(repos[1]['name'])
-  console.log(repos.map(r => {`<li>${r.name}</li>`}))
+  console.log(repos.map(console.log(r)))
 }
 
 getRepositories()
